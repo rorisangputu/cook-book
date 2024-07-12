7b7fa92f6e06ebd8f245e558bf2d98a5c3d177e4
Rorisang Putu <rorisangputu@gmail.com>
2024-07-12 12:42:34 +0200

    adding pages, setting up db connection

--- api/index.js
+++ api/index.js
@@ -12,9 +12,14 @@ dotenv.config();
 const connect = async () => {
     mongoose.set('strictQuery', true);
     try {
-        await mongoose.connect();
-        console.log('Connected to Databse')
+        await mongoose.connect(process.env.MONGO_CON);
+        console.log('Connected to Database')
     } catch (error) {
         console.log(error);
     }
 }
+
+app.listen(8800, () => {
+    connect();
+    console.log("Listening on port 8800");
+})
