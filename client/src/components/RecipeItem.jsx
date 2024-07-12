238aadc20707eb1adcd607bfdf578737e2d696fc
Rorisang Putu <rorisangputu@gmail.com>
2024-07-12 20:58:11 +0200

    rendering mock Recipe items

--- client/src/components/RecipeItem.jsx
+++ client/src/components/RecipeItem.jsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom'
 import foodpic from '../assets/foodpic.jpg'
 
 const RecipeItem = () => {
     return (
         <div>
-            <img className='h-48' src={foodpic} alt="" />
-            <h1 className='text-[23px]'>Shakshuka</h1>
+            <img className='h-48 rounded-2xl' src={foodpic} alt="" />
+            <Link>
+                <h1 className='text-[23px] mt-3 mb-1 font-medium'>Shakshuka</h1>
+            </Link>
             <p className='text-md text-[#626262]'>By Melissa Brown</p>
         </div>
     )
--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -3,8 +3,10 @@ import RecipeItem from './RecipeItem'
 
 const RecipeList = () => {
     return (
-        <div>
-            <RecipeItem />
+        <div className='w-full grid grid-cols-2 md:grid-cols-3'>
+            {[1, 2, 3,].map((recipe, index) => (
+                <RecipeItem key={index} />
+            ))}
         </div>
     )
 }
