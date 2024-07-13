965886dfaf346d0cf6f47eab5f274bc2143fba5f
Rorisang Putu <rorisangputu@gmail.com>
2024-07-13 22:50:47 +0200

    passing recipe data from db to client components

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -1,11 +1,23 @@
-
+import { useEffect, useState } from 'react'
 import RecipeItem from './RecipeItem'
 
 const RecipeList = () => {
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:8800/recipes/').then(res => {
+            res.json().then(recipes => {
+                setRecipes(recipes)
+            })
+        })
+    })
+
     return (
         <div className='w-full grid grid-cols-2 md:grid-cols-3 gap-7 items-center'>
-            {[1, 2, 3,].map((recipe, index) => (
-                <RecipeItem key={index} />
+            {recipes.length > 0 && recipes.map((recipe) => (
+                <div key={recipe._id}>
+                    <RecipeItem {...recipe} />
+                </div>
             ))}
         </div>
     )
