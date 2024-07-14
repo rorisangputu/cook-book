60e3e58c3ea59918f7611b8426363d68c857658c
Rorisang Putu <rorisangputu@gmail.com>
2024-07-14 23:27:42 +0200

    styled cta

--- client/src/components/CallToAct.jsx
+++ client/src/components/CallToAct.jsx
@@ -1,9 +1,9 @@
 const CallToAct = () => {
     return (
-        <div className="bg-img flex items-center justify-end h-[30vh] md:h-[40vh] w-full overflow-hidden">
-            <div className="p-4 text-white">
-                <h1 className="text-2xl font-bold">Your Content Here</h1>
-                <p className="text-lg">Additional content can go here.</p>
+        <div className="bg-img flex items-center justify-end h-[30vh] md:h-[30vh] w-full overflow-hidden">
+            <div className="p-4 bg-white rounded-2xl h-auto md:w-1/3 mx-10">
+                <h1 className="text-[#272727] text-2xl font-medium my-2">50+ Lunch Recipes To Create Very Quick And Easy</h1>
+                <p className="text-[#4d4d4d] text-md font-light">Find the perfect food ideas to for your lunch dates.</p>
             </div>
         </div>
     )
