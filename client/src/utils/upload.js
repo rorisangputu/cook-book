31ab0f98ca2e54f8dbef42e77075f8ca61a8f865
Rorisang Putu <rorisangputu@gmail.com>
2024-07-17 12:20:59 +0200

    adding proper env

--- client/src/utils/upload.js
+++ client/src/utils/upload.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+
 const upload = async (file) => {
     const data = new FormData();
     data.append("file", file);
     data.append("upload_preset", "recipe-cooks");
-    const uploadLink = 'https://api.cloudinary.com/v1_1/dslkrakk7/image/upload'
+    const uploadLink = import.meta.env.VITE_CLOUDINARY_URL;
+
     try {
         const res = await axios.post(uploadLink, data);
 
