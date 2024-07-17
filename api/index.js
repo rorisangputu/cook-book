918e3622f1928d24ce97b4cf141bca094076a4d5
Rorisang Putu <rorisangputu@gmail.com>
2024-07-17 16:56:22 +0200

    changed url

--- api/index.js
+++ api/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 //CORS Config
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://cook-book-client.onrender.com",
     credentials: true,
 }
 app.use(cors(corsOptions));
