6374e986596014a5964dbd56beb56c08b7a4c976
Rorisang Putu <rorisangputu@gmail.com>
2024-07-17 13:38:53 +0200

    Vercel baseURL for backend

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:8800",
+    baseURL: "https://cook-book-api-nu.vercel.app",
     withCredentials: true,
 });
 
