9382abfa589dcd7065650b0f382cae3953497c6c
Rorisang Putu <rorisangputu@gmail.com>
2024-07-28 01:12:02 +0200

    changed hero text and img sizing on recipe item

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,8 +7,8 @@ const Hero = () => {
             <div className='w-[90%] md:container lg:w-[90%] xl:container mx-auto flex items-center lg:justify-between '>
                 <div className='text-white py-5 flex flex-col text-center lg:text-left lg:w-3/5 '>
                     <h1 className='text-[50px] sm:text-[65px] xl:mr-56 font-semibold'>The Easiest Way To Make Your Favourite Meal</h1>
-                    <p className='my-4 mx-9 md:mx-0 xl:mr-48  text-[16px] text-[#f2f2f2]'>New school twists to old school recipes! Discover over 1000+ recipes
-                        your hand. The easiest way to cook.
+                    <p className='my-4 mx-9 md:mx-0 xl:mr-48  text-[16px] text-[#f2f2f2]'>New school twists to old school recipes! Discover recipes
+                        with Taste Book. The easiest way to cook.
                     </p>
                     <Link to={'/recipes'}>
                         <button className='px-6 py-3 my-4  text-[16px] rounded-xl bg-[#1d9451]'>Explore Recipes</button>
