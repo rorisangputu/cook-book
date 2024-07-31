93de557a53ef5729fe89beac45216bc10b8f0ad1
Rorisang Putu <rorisangputu@gmail.com>
2024-07-31 20:55:58 +0200

    adding new frontend link

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(cookieParser());
 
 //CORS Config
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: "https://taste-book-client.onrender.com/",
     credentials: true,
 }
 app.use(cors(corsOptions));
