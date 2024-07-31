dbb9a04a5eebc1babc407357641585836e9029c1
Rorisang Putu <rorisangputu@gmail.com>
2024-07-31 21:34:11 +0200

    console log jwt

--- api/middleware/jwt.js
+++ api/middleware/jwt.js
@@ -6,6 +6,8 @@ import createError from "../utils/createError.js";
 export const verifyToken = (req, res, next) => {
     const token = req.cookies.accessToken;
     const key = process.env.JWT_KEY 
+      console.log('Token:', token); // Log token for debugging
+    console.log('Key:', key); // Log key for debugging
     if (!token) return res.status(401).json({ message: "You are not authenticated." });
 
     jwt.verify(token, key, async (err, payload) => {
