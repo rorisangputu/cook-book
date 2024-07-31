6b000160b856be9f33e83b56fba7196b8de6b15b
Rorisang Putu <84104112+rorisangputu@users.noreply.github.com>
2024-07-31 21:00:35 +0200

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // import { useNavigate } from 'react-router-dom';
 
 const newRequest = axios.create({
-    baseURL: 'https://taste-book-api.onrender.com/',
+    baseURL: 'https://taste-book-api.onrender.com',
     withCredentials: true
 });
 
